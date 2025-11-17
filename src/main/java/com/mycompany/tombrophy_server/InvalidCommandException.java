5db507a662d61b0b7b304e652d674bb7d2cc4265
Tom Brophy <45319812+TomBrophy101@users.noreply.github.com>
2025-11-17 18:43:48 +0000

    Add files via upload

---
+++ src/main/java/com/mycompany/tombrophy_server/InvalidCommandException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tombrophy_server;
+
+/**
+ *
+ * @author tombr
+ */
+public class InvalidCommandException extends Exception {
+    public InvalidCommandException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/mycompany/tombrophy_server/TomBrophy_ClientRun.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tombrophy_server;
+
+import java.io.*;
+import java.net.*;
+/**
+ *
+ * @author tombr
+ */
+public class TomBrophy_ClientRun implements Runnable {
+    Socket client_link = null;
+    String EventBoardClientID;
+    
+    public TomBrophy_ClientRun(Socket connection, String eID) {
+        this.client_link = connection;
+        EventBoardClientID = eID;
+    }
+    
+    @Override
+    public void run () {
+        try {
+            BufferedReader in = new BufferedReader( new InputStreamReader(client_link.getInputStream()));
+            PrintWriter out = new PrintWriter(client_link.getOutputStream(),true);
+            
+            String message = in.readLine();
+            System.out.println("Message received from client:" + EventBoardClientID + " " + message);
+            out.println("Echo Message: " + message);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        
+        finally
+        {
+            try {
+                System.out.println("\n* Closing connection with the client " + EventBoardClientID + " ... ");
+                client_link.close();
+            }
+            catch(IOException e) {
+                System.out.println("Disconnection failed!");
+            }
+        }
+    }
+}
